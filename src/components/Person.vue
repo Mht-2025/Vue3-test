f2993a57863134728ff107206f1bb7d3856074e4
mhtkk <2328404044@qq.com>
2025-04-19 18:50:59 +0800

    setup概述（组合API）

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,34 +2,43 @@
   <div class="person">
     <h2>姓名：{{name}}</h2>
     <h2>年龄：{{age}}</h2>
+
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年龄</button>
     <button @click="showTel">查看联系方式</button>
+
   </div>
 </template>
 
 <script lang="ts">
   export default {
     name:'Person-info',
-    data(){
-      return {
-        name:'张三',
-        age:18,
-        tel:'13888888888'
+    setup(){
+     //变量
+      let name = '张三' //此时的name不是响应式数据
+      let age = 19//此时age不是响应式数据
+      const tel = '13888888888'
+
+      //方法
+      function changeName (){
+        name = 'zhang-san'
+        console.log(name);
       }
-    },
-    methods:{
-      // 修改姓名
-      changeName(){
-        this.name = 'zhang-san'
-      },
-      // 修改年龄
-      changeAge(){
-        this.age += 1
-      },
-      // 展示联系方式
-      showTel(){
-        alert(this.tel)
+      function changeAge (){
+        age += 1
+        console.log(age);
+
+      }
+      function showTel (){
+        alert(tel)
+      }
+      return {
+        name,
+        age,
+        tel,
+        changeName,
+        changeAge,
+        showTel
       }
     }
   }
