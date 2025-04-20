122aec808eac352d7c297a61c0b3d1048800e76a
mhtkk <2328404044@qq.com>
2025-04-20 21:26:47 +0800

    ref属性

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="app">
-    <PersonInfo/>
+    <h2 ref="title2">你好</h2>
+    <button @click="test">点我输出你好</button>
+    <Personinfo ref="ren"/>
   </div>
 </template>
+<!--
+在 <script setup> 中，
+ 通过 import PersonInfo from './components/PersonInfo.vue' 引入了 PersonInfo 组件。
+使用 <PersonInfo /> 标签将该组件嵌套到当前组件（App.vue）的模板中
+-->
+<script lang="ts" setup name="App">
+  import Personinfo from './components/PersonInfo.vue'
+  import { ref } from 'vue'
+  const title2 = ref()
+  const ren = ref()
+  function test(){
+    console.log(title2.value);
+    console.log(ren.value);
 
-<script lang="ts">
-  import PersonInfo from './components/PersonInfo.vue'
-  export default {
-    name:'App', //组件名
-    components:{PersonInfo} //注册组件
   }
 </script>
 
--- src/components/PersonInfo.vue
+++ src/components/PersonInfo.vue
@@ -1,56 +1,36 @@
 <template>
   <div class="person">
-    <h1>watchEffect</h1>
-    <h2 id="demo">水温：{{ temp }} °0</h2>
-    <h2>水位：{{ height }}cm</h2>
-    <button @click="changTemp">水温+10</button>
-    <button @click="changHeight">水位+10</button>
+    <h1>ref属性</h1>
+    <h1>中国</h1>
+     <!-- 为该 <h1> 元素创建了一个引用 名称为 title2
+      title2算是一个容器，存储当前的DOM元素<h2>
+      -->
+    <h1 ref="title2">北京</h1>
+    <h1>广州</h1>
+    <button @click="shouH2">点击输出h2</button>
   </div>
 </template>
 
 <script setup lang="ts" name="PersonInfo">
-    import { ref,watch, watchEffect } from 'vue';
-    const temp = ref(0);
-    const height = ref(0);
-    function changTemp() {
-      temp.value += 10;
-    }
-    function changHeight() {
-      height.value += 10;
-    }
-    // watchEffect(() => {
-    //   if (temp.value > 50 ) {
-    //     alert('水温过高');
-    //   }
-    // });
-    // watchEffect(() => {
-    //   if (height.value > 40 ) {
-    //     alert('水位过高');
-    //   }
-    // })
-
-    // 用watch实现，需要明确的指出要监视：temp、height
-   watch([temp,height],(value)=>{
-    // 从value中获取最新的temp值、height值
-    const [newTemp,newHeight] = value
-    // 室温达到50℃，或水位达到20cm，立刻联系服务器
-    if(newTemp >= 50 || newHeight >= 20){
-      console.log('联系服务器')
-    }
-  })
-
-    // 用watchEffect实现，不用
-    const stopWtach = watchEffect(() => {
-      // 室温达到50℃，或水位达到20cm，立刻联系服务器
-      if (height.value > 40 || temp.value > 50 ) {
-        console.log('水位过高或水温过高,停止监视');
-        // 停止监视
-        stopWtach();
-      }
-    })
+  import {ref,defineExpose} from 'vue'
+  //创建一个响应式引用
+  //如果某个元素使用了 ref="title2"，
+  // Vue 会在组件挂载后将该 DOM 元素赋值给 title2.value。
+  const title2 =ref()
+  const a = ref(0)
+  const b = ref(1)
+  const c = ref(2)
+  function shouH2(){
+    //通过 title2.value，可以访问到绑定的 DOM 元素及其属性和方法
+    //title2.value 将指向 <h1> 元素
+    console.log(title2.value)
+  }
+  //defineExpose 暴露组件的属性和方法,允许组件的父组件访问a,b,c
+  defineExpose({a,b,c})
 </script>
-
+//scoped局部样式：样式只在当前组件生效
 <style scoped>
+
   .person {
     background-color: skyblue;
     box-shadow: 0 0 10px;
