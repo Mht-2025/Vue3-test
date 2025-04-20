8e8df2ec4dfe8597522d713e97a655a2b4c9c8cd
mhtkk <2328404044@qq.com>
2025-04-20 14:28:06 +0800

    computed计算属性方法

--- src/components/PersonInfo.vue
+++ src/components/PersonInfo.vue
@@ -1,50 +1,38 @@
 <template>
   <div class="person">
-    <h2>姓名：{{person.name}}</h2>
-    <h2>年龄：{{person.age}}</h2>
-    <h2>性别：{{person.gender}}</h2>
-    <button @click="changeName">修改名字</button>
-    <button @click="changeAge">修改年龄</button>
-    <button @click="changeGender">修改性别</button>
+   姓：<input type="text" v-model="firstName"><br>
+   名：<input type="text" v-model="LastName"><br>
+   <!-- <button @click="getName">姓名</button>{{ AllName }} -->
+    <span>全名：{{ AllName }}</span>
+    <button @click="changeName">改名</button>
   </div>
 </template>
 
 //组合式API
 <script setup lang="ts" name="PersonInfo">
-import { reactive,toRef,toRefs } from 'vue'
-  //数据
-  const person = reactive({
-    name: '张三',
-    age: 18,
-    gender: '男'
-  })
-  // 解构 const {name,age,gender} = person相当于
-  // const name = person.name
-  // const age = person.age
-  // const gender = person.gender
-  // 以上定义的（也就是结构出来的）的name,age,gender均不是响应式的
-  // toRefs方法可以将person对象中的属性转换为响应式的ref对象
-  // const {name,age,gender} = toRefs(person)
-  // 加上toRefs方法后，name,age,gender都是响应式的
-  // 相当于 const name = ref(person.name)
-  // toRef方法可以将person对象中的属性转换为响应式的ref对象
-  // toRef则是取出单个的转换并且的响应式数据，toRefs则将person对象中的所有属性转换为响应式的ref对象
-  const {name,age} = toRefs(person)
-  const gd = toRef(person,'gender')
-  // 方法
-  const changeName = () => {
-    name.value = '李四'
+import { ref ,computed} from 'vue';
+const firstName = ref('张');
+const LastName = ref('三');
+
+ // 计算属性——只读取，不修改
+  /* let fullName = computed(()=>{
+    return firstName.value + '-' + lastName.value
+  }) */
+
+const AllName = computed({
+  get(){
+    return firstName.value +"-"+ LastName.value;
+  },
+  set(val){
+    console.log("有人给名啦",val);
+    //以“-”分割并分别取出
+    firstName.value = val.split('-')[0];
+    LastName.value = val.split('-')[1];
   }
-
-  const changeAge = () => {
-    age.value += 2
+});
+  function changeName(){
+    AllName.value = 'hao-四';
   }
-
-  const changeGender = () => {
-    gd.value = '女'
-  }
-
-
 </script>
 
 <style scoped>
