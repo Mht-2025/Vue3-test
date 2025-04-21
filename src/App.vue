2ca14a39914f4e0c68c2ae7a3e6b17b23a68ae3d
mhtkk <2328404044@qq.com>
2025-04-21 14:56:15 +0800

    生命周期

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
 <template>
-    <PersonInfo :list="persons" />
+  <Person v-if="isShow" />
 </template>
-<!--
-在 <script setup> 中，
- 通过 import PersonInfo from './components/PersonInfo.vue' 引入了 PersonInfo 组件。
-使用 <PersonInfo /> 标签将该组件嵌套到当前组件（App.vue）的模板中
--->
+
 <script lang="ts" setup name="App">
-  import PersonInfo from './components/PersonInfo.vue'
-  import { type Persons} from './types/index'
-  import {reactive} from 'vue'
-  const persons = reactive<Persons>([
-     {id:'e98219e12',name:'张三',age:18},
-      {id:'e98219e13',name:'李四',age:19},
-       {id:'e98219e14',name:'王五',age:20}
-     ])
+import Person from "./components/Person.vue";
+import { ref, onMounted } from "vue";
+
+const isShow = ref(true);
+
+// 挂载完毕
+onMounted(() => {
+  console.log("父---挂载完毕");
+});
 </script>
-
-<style>
-
-</style>
