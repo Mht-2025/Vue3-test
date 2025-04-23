6782b89f90b9462252bc2646ae74ff4d45e20782
mhtkk <2328404044@qq.com>
2025-04-23 18:54:02 +0800

    搭建pinia环境

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-// 引入createApp用于创建应用
 import {createApp} from 'vue'
-// 引入App根组件
 import App from './App.vue'
-
-// 创建一个应用
+// 第一步 引入pinia
+import {createPinia} from 'pinia'
 const app = createApp(App)
-
+// 第二步 创建pinia
+const pinia = createPinia()
+// 第三步 安转pinia
+app.use(pinia)
 app.mount('#app')
